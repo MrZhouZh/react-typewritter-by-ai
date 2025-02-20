199943b13b6b395484e8a6745125875dfd36daeb
zhouzhikai <zhouzhikai@smm.cn>
2025-02-20 14:19:54 +0800

    fix(types): jest type

--- src/components/TypeWriter.test.tsx
+++ src/components/TypeWriter.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, screen, act } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import { TypeWriter } from './TypeWriter';
 
 jest.setTimeout(10000);
